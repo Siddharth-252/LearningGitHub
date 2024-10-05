1e82baad1e312952055dfbf18c313021c8f5df02
Sri CHaRan <sricharanchittineni@gmail.com>
2024-10-05 08:08:12 +0530

    kappa is liked by hyundai

---
+++ kappa.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout<<100-99<<endl;
+    return 0;
+}
--- one.cpp
+++
@@ -1,8 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int main(){
-    cout<<"Tryna' learn Git and Github....!"<<endl;
-    cout<<"Learning in good pace....?"<<endl;
-    return 0;
-}
