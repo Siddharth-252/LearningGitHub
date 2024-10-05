e8eac2e40d5b31b475941ecf7338c3c1d1bcc4bb
Sri CHaRan <sricharanchittineni@gmail.com>
2024-10-05 07:18:13 +0530

    first commit

---
+++ cp.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout<<"I'm a disco dancer."<<endl;
+    return 0;
+}
--- one.cpp
+++
@@ -1,7 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int main(){
-    cout<<"Tryna' learn Git and Github....!"<<endl;
-    return 0;
-}
